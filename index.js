816f3a18451f4bc632e7226bae51daa39f48b2f7
Lakhan Nad <lakhannad1999@gmail.com>
2020-11-24 16:57:51 +0530

    Update Port

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/",(req,res)=>{
     res.sendFile(path.join(__dirname, path.join("public", "index.html")));
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Server Listening on port: ${port}`);
 });
